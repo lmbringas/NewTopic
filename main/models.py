1308cffd8c7d19c25e1efc8d86f0e73a73f9ab25
lmbringas <lm.bringas98@gmail.com>
2016-05-06 11:48:06 -0300

    creat models author post comment

--- main/models.py
+++ main/models.py
@@ -1,5 +1,24 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Author(models.Model):
+    user = models.OneToOneField(User)
+    nationality = models.CharField(max_length=32,
+                                  default="")
+
+class Post(models.Model):
+    title = models.CharField('Titulo',max_length=32)
+    content = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    publicated = models.BooleanField()
+    author = models.ForeignKey(Author)
+
+class Comment(models.Model):
+    author = models.ForeignKey(User,null=True,blank=True)
+    comment = models.TextField()
+    post = models.ForeignKey(Post)
+    date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField()
