a5e35a0e76df185091a19128e01a121d57e2e42f
lmbringas <lm.bringas98@gmail.com>
2016-05-13 08:09:24 -0300

    add views home post

--- main/views.py
+++ main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, render, redirect
+from django.template import RequestContext
 # Create your views here.
+from models import Post
+
+def home(request):
+    context = RequestContext(request)
+    posts = Post.objects.all()
+    context['usuario_actual'] = request.user
+    context['posts'] = posts
+    return render_to_response("index.html", context)
+
+def post(request,id_post):
+    return render_to_response("post.html",context)
