1e47e050dd8fca21d7f5dd8ba062454b487544b5
lautaro.bringas <lm.bringas98@gmail.com>
2016-08-29 11:26:35 -0300

    Merge remote-tracking branch 'git/master' into develope
    # Conflicts:
    #	.gitignore

--- main/models.py
+++ main/models.py
@@ -4,28 +4,31 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Create your models here.
 class Author(models.Model):
     user = models.OneToOneField(User)
     nationality = models.CharField(max_length=32,
-                                  default="")
+                                   default="")
 
     def __str__(self):
         return self.user.get_username()
 
+
 class Post(models.Model):
-    title = models.CharField('Titulo',max_length=32)
+    title = models.CharField('Titulo', max_length=32)
     content = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    #photo = models.ImageField(upload_to="media/", blank=True)
+    # photo = models.ImageField(upload_to="media/", blank=True)
     publicated = models.BooleanField()
     author = models.ForeignKey(Author)
 
     def __str__(self):
         return self.title
 
+
 class Comment(models.Model):
-    author = models.ForeignKey(User,null=True,blank=True)
+    author = models.ForeignKey(User, null=True, blank=True)
     comment = models.TextField()
     post = models.ForeignKey(Post)
     date = models.DateTimeField(auto_now_add=True)
--- main/views.py
+++ main/views.py
@@ -1,16 +1,15 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.shortcuts import render_to_response, render, redirect
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-from models import Post , Comment , Author
+from models import Post, Comment, Author
 from django.contrib.auth.models import User
-import json
+
+
 # Create your views here.
 
 
 def home(request):
-    '''
+    """
     codigo para ajax
     TOTAL = 5
     OFFSET = int(request.GET.get('offset', 0))
@@ -25,7 +24,7 @@ def home(request):
                 })
         data = json.dumps(json_list)
         return HttpResponse(data,content_type='application/json')
-    '''
+    """
     context = RequestContext(request)
     if request.method == 'POST':
         title = request.POST["title"]
@@ -34,33 +33,35 @@ def home(request):
         nPost.title = title
         nPost.content = content
         nPost.publicated = True
-        user = User.objects.get(pk = request.user.id)
+        user = User.objects.get(pk=request.user.id)
         nPost.author = Author.objects.get(user=user)
         nPost.save()
     posts = Post.objects.all()
     context['posts'] = posts
     return render_to_response("index.html", context)
 
+
 @login_required(login_url="/log")
-def post(request,id_post):
+def post(request, id_post):
     context = RequestContext(request)
     post = Post.objects.get(pk=id_post)
     if request.method == 'POST':
         comment = request.POST['comment']
         ncomment = Comment()
-        ncomment.author = User.objects.get(pk = request.user.id)
+        ncomment.author = User.objects.get(pk=request.user.id)
         ncomment.post = post
         ncomment.comment = comment
         ncomment.approved = True
         ncomment.save()
-    comments = Comment.objects.all().filter(post=id_post) #select * from commets where id_post = id_post
+    comments = Comment.objects.all().filter(post=id_post)  # select * from comments where id_post = id_post
     context['post'] = post
     context['comments'] = comments
-    return render_to_response("post.html",context)
+    return render_to_response("post.html", context)
+
 
 def ownPost(request):
     context = RequestContext(request)
-    user = User.objects.get(pk = request.user.id)
+    user = User.objects.get(pk=request.user.id)
     author = Author.objects.get(user=user)
     posts = Post.objects.filter(author=author)
     context['posts'] = posts
